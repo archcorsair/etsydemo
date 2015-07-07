577ae0f1ebd142797637529a186928a556ae4e63
Daniel Shneyder <archcorsair@gmail.com>
2015-07-07 10:05:58 -0700

    Finished setup for Dropbox

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,16 +1,11 @@
-# require 'paperclip/media_type_spoof_detector'
-# module Paperclip
-#   class MediaTypeSpoofDetector
-#     def spoofed?
-#       false
-#     end
-#   end
-# end
-
 class Listing < ActiveRecord::Base
-	has_attached_file :image, :styles => { :medium => "200x", :thumb => "100x100>" }, :default_url => "default.png",
-					  :storage => :dropbox,
-    				  :dropbox_credentials => Rails.root.join("config/dropbox.yml")
-	validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
-
+	if Rails.evn.development?
+		has_attached_file :image, :styles => { :medium => "200x", :thumb => "100x100>" }, :default_url => "default.png"
+	else
+		has_attached_file :image, :styles => { :medium => "200x", :thumb => "100x100>" }, :default_url => "default.png",
+						  :storage => :dropbox,
+	    				  :dropbox_credentials => Rails.root.join("config/dropbox.yml"),
+	    				  :path => ":style/:id_:filename"
+		validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
+	end
 end
