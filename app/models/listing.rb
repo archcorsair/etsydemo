59239bf41ed80451fba720bc6af2ef75591d1458
Daniel Shneyder <archcorsair@gmail.com>
2015-07-07 09:51:25 -0700

    Added fiagro

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,14 +1,16 @@
-require 'paperclip/media_type_spoof_detector'
-module Paperclip
-  class MediaTypeSpoofDetector
-    def spoofed?
-      false
-    end
-  end
-end
+# require 'paperclip/media_type_spoof_detector'
+# module Paperclip
+#   class MediaTypeSpoofDetector
+#     def spoofed?
+#       false
+#     end
+#   end
+# end
 
 class Listing < ActiveRecord::Base
-	has_attached_file :image, :styles => { :medium => "200x", :thumb => "100x100>" }, :default_url => "default.png"
+	has_attached_file :image, :styles => { :medium => "200x", :thumb => "100x100>" }, :default_url => "default.png",
+					  :storage => :dropbox,
+    				  :dropbox_credentials => Rails.root.join("config/dropbox.yml")
 	validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
 
 end
